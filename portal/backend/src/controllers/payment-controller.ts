86cf2c0cf24691a1f449e96923d01a9924ff6c54
Wishva Kalhara <vishvakalhara@gmail.com>
2024-12-22 10:10:45 +0530

    stripe url

--- portal/backend/src/controllers/payment-controller.ts
+++ portal/backend/src/controllers/payment-controller.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Response, Request } from "express";
+import Stripe from "stripe";
 import AppError from "../utils/AppError";
+import applicationSchema from "../schemas/application-schema";
 
 export const createCheckoutLink = async (
     req: Request,
@@ -7,13 +9,43 @@ export const createCheckoutLink = async (
     next: NextFunction
 ) => {
     try {
-        // Create Checkout session
-        // Get public key from req body
-        // Get price id from req body
+        if (!req.body.stripeSecret || !req.body.appSecret) {
+            return next(
+                new AppError("Stripe secret key and App Secret required", 400)
+            );
+        }
 
-        res.redirect(200, "https://www.youtube.com/");
-    } catch (error) {
-        console.error(error);
+        const app = await applicationSchema.findOne({
+            appSecret: req.body.appSecret,
+            isActive: true,
+        });
+        if (!app) {
+            return next(new AppError("Application not found", 404));
+        }
+
+        const stripe = new Stripe(req.body.stripeSecret);
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: [
+                {
+                    price_data: {
+                        currency: "usd",
+                        product_data: {
+                            name: `${app.name}-${app.plans[0].planName}(${app.plans[0].validityInDays} days)`,
+                        },
+                        unit_amount: app.plans[0].price * 100, // Price in cents ($20.00)1
+                    },
+                    quantity: 1,
+                },
+            ],
+            mode: "payment", // Use 'payment' for one-time payments
+            success_url: "https://yourdomain.com/success", // Replace with your success URL
+            cancel_url: "https://yourdomain.com/cancel", // Replace with your cancel URL
+        });
+
+        res.json({ url: session.url });
+    } catch (err) {
+        console.error(err);
         next(new AppError("Unknown Error Occured!", 500));
     }
 };
