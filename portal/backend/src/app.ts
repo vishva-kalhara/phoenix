59222281df7a17de9af32b69afc2258a1d55ed3f
Wishva Kalhara <vishvakalhara@gmail.com>
2025-01-06 19:02:58 +0530

    user creting bug fixed

--- portal/backend/src/app.ts
+++ portal/backend/src/app.ts
@@ -12,7 +12,12 @@ import cors from "cors";
 export const createApp = () => {
     const app = express();
 
-    app.use(cors());
+    const corsOptions = {
+        // origin: "http://localhost:5173", // Replace this with the exact origin of your frontend
+        credentials: true, // Allow credentials (cookies, headers, etc.)
+    };
+    app.use(cors(corsOptions));
+    app.options("*", cors());
 
     app.use(express.json({ limit: "10kb" }));
 
